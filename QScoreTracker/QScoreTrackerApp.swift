89477f6b680885373ae40c2d958d42d9ef8c4c5c
CongQuynh <congquynh0206@gmail.com>
2026-05-21 17:02:24 +0700

    Initial Commit

---
+++ QScoreTracker/QScoreTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QScoreTrackerApp.swift
+//  QScoreTracker
+//
+//  Created by Nguyen Cong Quynh on 21/5/26.
+//
+
+import SwiftUI
+
+@main
+struct QScoreTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
